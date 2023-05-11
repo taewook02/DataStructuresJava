c92a0e4057e690563b0ca32352cdf0bd451f6f78
taewook02 <taewook02@mju.ac.kr>
2023-05-11 22:29:11 +0900

    changed format

--- DataStructures/Trees/BinarySearchTree/Main.java
+++ DataStructures/Trees/BinarySearchTree/Main.java
@@ -3,7 +3,7 @@ package DataStructures.Trees.BinarySearchTree;
 public class Main {
 
     public static void main(String[] args) {
-        
+
         System.out.println();
         BST<Integer, String> bst = new BST<>(50, "Apple");
 
